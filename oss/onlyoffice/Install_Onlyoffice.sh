fbb98f700e9b798369c57094b27bff28e021dff7
Inyong Ham <iyham@naver.com>
2015-12-07 17:33:11 +0900

    Update and rename install_onlyoffice.sh to Install_Onlyoffice.sh

--- oss/onlyoffice/Install_Onlyoffice.sh
+++ oss/onlyoffice/Install_Onlyoffice.sh
@@ -1,3 +1,5 @@
+###  Please see the list of all supported kernel versions http://onlyo.co/1PABPEI
+
 sudo wget http://download.onlyoffice.com/repo/onlyoffice.key 
 sudo apt-key add onlyoffice.key
 echo "deb http://download.onlyoffice.com/repo/debian squeeze main" | sudo tee -a /etc/apt/sources.list
