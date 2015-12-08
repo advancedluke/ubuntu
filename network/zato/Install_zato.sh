3cb785983d6d38386de1c402bd6e1323d2a7aaad
Inyong Ham <iyham@naver.com>
2015-12-08 14:58:13 +0900

    Update Install_zato.sh

--- network/zato/Install_zato.sh
+++ network/zato/Install_zato.sh
@@ -1,6 +1,5 @@
 # https://zato.io/docs/admin/guide/install-ubuntu.html
-sudo apt-get install apt-transport-https
-sudo apt-get install python-software-properties
+sudo apt-get install apt-transport-https python-software-properties -y
 
 curl -s https://zato.io/repo/zato-0CBD7F72.pgp.asc | sudo apt-key add -
 sudo apt-add-repository https://zato.io/repo/stable/2.0/ubuntu
