face9e7c8f56a42b0aa75eb358acf39b0796ecbb
Inyong Ham <iyham@naver.com>
2015-12-08 14:53:18 +0900

    Create Install_zato.sh

---
+++ network/zato/Install_zato.sh
@@ -0,0 +1,8 @@
+sudo apt-get install apt-transport-https
+sudo apt-get install python-software-properties
+
+curl -s https://zato.io/repo/zato-0CBD7F72.pgp.asc | sudo apt-key add -
+sudo apt-add-repository https://zato.io/repo/stable/2.0/ubuntu
+sudo apt-get update
+
+sudo apt-get install zato
